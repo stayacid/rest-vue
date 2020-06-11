c11995d3975483c69844437260496200fb821683
stayacid <stayacid@gmail.com>
2020-06-11 15:17:56 +0300

    done

--- app.js
+++ app.js
@@ -1,6 +1,43 @@
 const express = require('express')
 const path = require('path')
+const {v4} = require('uuid')
 const app = express()
+
+// database
+let CONTACTS = [{
+  id: v4(), name: 'Adam', value: '8-800-2000-600', marked: false
+}]
+
+// to work with requests
+app.use(express.json())
+
+// on GET request return CONTACTS
+app.get('/api/contacts', (req, res) => {
+  // simulate loading
+  setTimeout(() => {
+    res.status(200).json(CONTACTS)
+  }, 1500);
+})
+
+// POST
+app.post('/api/contacts', (req, res) => {
+  const contact = {...req.body, id: v4(), marked: false}
+  CONTACTS.push(contact)
+  res.status(201).json(contact)
+})
+
+// DELETE
+app.delete('/api/contacts/:id', (req, res) => {
+  CONTACTS = CONTACTS.filter(c => c.id !== req.params.id)
+  res.status(200).json({message: 'Contact was deleted'}) // status 200 is not necessary for express
+})
+
+// PUT
+app.put('/api/contacts/:id', (req, res) => {
+  const idx = CONTACTS.findIndex(c => c.id === req.params.id)
+  CONTACTS[idx] = req.body
+  res.json(CONTACTS[idx])
+})
 
 // use static folder
 app.use(express.static(path.resolve(__dirname, 'client')))
--- client/client.js
+++ client/client.js
@@ -1,36 +1,82 @@
-import Vue from 'https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js'
+import Vue from "https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.esm.browser.js";
+
+Vue.component("loader", {
+  template: `
+    <div style="display: flex; justify-content: center; align-items: center">
+      <div class="spinner-border" role="status">
+        <span class="sr-only">Loading...</span>
+      </div>
+    </div>
+  `,
+});
 
 new Vue({
-  el: '#app',
+  el: "#app",
   data() {
     return {
+      loading: false,
       form: {
-        name: '',
-        value: ''
+        name: "",
+        value: "",
       },
-      contacts: [{
-        id: 1, name: 'Adam', value: '8-800-2000-600', marked: false
-      }]
-    }
+      contacts: [],
+    };
   },
   computed: {
     canCreate() {
-      return this.form.value.trim() && this.form.name.trim()
-    }
+      return this.form.value.trim() && this.form.name.trim();
+    },
   },
   methods: {
-    createContact() {
-      const {...contact} = this.form
-      this.contacts.push({...contact, id: Date.now(), marked: false})
-      
-      this.form.name = this.form.value = ''
+    async createContact() {
+      const { ...contact } = this.form;
+
+      const newContact = await request("/api/contacts", "POST", contact)
+      this.contacts.push(newContact);
+
+      this.form.name = this.form.value = "";
     },
-    markContact(id) {
-      const contact = this.contacts.find(c => c.id === id)
-      contact.marked = true
+
+    async markContact(id) {
+      const contact = this.contacts.find((c) => c.id === id);
+      const updated = await request(`/api/contacts/${id}`, "PUT", {
+        ...contact,
+        marked: true
+      })
+      contact.marked = updated.marked;
     },
-    removeContact(id) {
-      this.contacts = this.contacts.filter(c => c.id !== id)
+
+    async removeContact(id) {
+      await request(`/api/contacts/${id}`, 'DELETE')
+      this.contacts = this.contacts.filter((c) => c.id !== id);
+    },
+  },
+  async mounted() {
+    this.loading = true;
+    // '/' === 'http://localhost:3000'
+    this.contacts = await request("/api/contacts");
+    this.loading = false;
+  },
+});
+
+async function request(url, method = "GET", data = null) {
+  try {
+    const headers = {};
+    let body;
+
+    if (data) {
+      headers["Content-Type"] = "application/json";
+      body = JSON.stringify(data);
     }
+
+    const response = fetch(url, {
+      method,
+      headers,
+      body,
+    });
+
+    return (await response).json();
+  } catch (e) {
+    console.warn("Error: ", e.message);
   }
-})
+}
