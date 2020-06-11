a1893aabe0190ffb9b5a2b1abeae606ad3b8e0ec
stayacid <stayacid@gmail.com>
2020-06-11 10:49:05 +0300

    add some logic

--- client/client.js
+++ client/client.js
@@ -8,15 +8,29 @@ new Vue({
         name: '',
         value: ''
       },
-      contacts: []
+      contacts: [{
+        id: 1, name: 'Adam', value: '8-800-2000-600', marked: false
+      }]
+    }
+  },
+  computed: {
+    canCreate() {
+      return this.form.value.trim() && this.form.name.trim()
     }
   },
   methods: {
     createContact() {
       const {...contact} = this.form
-      this.contacts.push({...contact, id: Date.now()})
+      this.contacts.push({...contact, id: Date.now(), marked: false})
       
       this.form.name = this.form.value = ''
+    },
+    markContact(id) {
+      const contact = this.contacts.find(c => c.id === id)
+      contact.marked = true
+    },
+    removeContact(id) {
+      this.contacts = this.contacts.filter(c => c.id !== id)
     }
   }
 })
